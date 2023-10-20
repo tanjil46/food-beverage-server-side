43c41e96bbe33874893d63fff307a5fc9cb6e993
tanjil hoassain <tanjilt46@gmail.com>
2023-10-20 01:52:27 -0700

    facing problems

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
  const foodAndBeverageProduct=client.db('foodDB').collection('product')
  const myCartAddedProduct=client.db('AddeCartDB').collection('cart')
